49369b356a950600f01b648a012ab8257e847666
WeiHung Lin <b06207053@ntu.edu.tw>
2023-04-08 13:44:02 +0800

    Initial script stub

---
+++ Python_scripts.py
@@ -0,0 +1,21 @@
+from sklearn import preprocessing as pre
+from argparse import ArgumentParser
+from scipy import signal
+
+
+import scipy.io as sio
+import neurokit2 as nk
+import pandas as pd
+import numpy as np
+import math
+
+
+
+def driver():
+    description = "Biosignal Emotions project BHS 2020."
+    parser = ArgumentParser(__file__, description)
+
+# main scripting details could go here
+if __name__ == "__main__":
+   driver()
+ 
