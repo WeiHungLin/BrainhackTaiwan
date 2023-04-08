64b57f8c3ddcb9e66fc94693d6953c5bee0bf087
WeiHung Lin <b06207053@ntu.edu.tw>
2023-04-08 14:00:25 +0800

    Add several defined functions for ECG preprocessing

--- Python_scripts.py
+++ Python_scripts.py
@@ -10,6 +10,52 @@ import numpy as np
 import math
 
 
+def preprocessing_and_feature_extraction_ECG(file_name_csv,raw):
+    data_ECG={}
+    for participant in range(0,23):
+        for video in range(0,18):
+            # load raw baseline and stimuli data for left and right
+            basl_l=raw['DREAMER'][0,0]['Data'][0,participant]['ECG'][0,0]['baseline'][0,0][video,0][:,0]
+            stim_l=raw['DREAMER'][0,0]['Data'][0,participant]['ECG'][0,0]['stimuli'][0,0][video,0][:,0]
+            basl_r=raw['DREAMER'][0,0]['Data'][0,participant]['ECG'][0,0]['baseline'][0,0][video,0][:,1]
+            stim_r=raw['DREAMER'][0,0]['Data'][0,participant]['ECG'][0,0]['stimuli'][0,0][video,0][:,1]
+            # process with neurokit
+            ecg_signals_b_l,info_b_l=nk.ecg_process(basl_l,sampling_rate=256)
+            ecg_signals_s_l,info_s_l=nk.ecg_process(stim_l,sampling_rate=256)
+            ecg_signals_b_r,info_b_r=nk.ecg_process(basl_r,sampling_rate=256)
+            ecg_signals_s_r,info_s_r=nk.ecg_process(stim_r,sampling_rate=256)
+            # divide stimuli features by baseline features
+            # would be interesting to compare classification accuracy when we
+            # don't do this
+            features_ecg_l=nk.ecg_intervalrelated(ecg_signals_s_l)/nk.ecg_intervalrelated(ecg_signals_b_l)
+            features_ecg_r=nk.ecg_intervalrelated(ecg_signals_s_r)/nk.ecg_intervalrelated(ecg_signals_b_r)
+            # average left and right features
+            # would be interesting to compare classification accuracy when we
+            # rather include both left and right features
+            features_ecg=(features_ecg_l+features_ecg_r)/2
+            if not len(data_ECG):
+                data_ECG=features_ecg
+            else:
+                data_ECG=pd.concat([data_ECG,features_ecg],ignore_index=True)
+
+
+def preprocessing(raw, feature):
+    overall=signal.firwin(9,[0.0625,0.46875],window='hamming')
+    theta=signal.firwin(9,[0.0625,0.125],window='hamming')
+    alpha=signal.firwin(9,[0.125,0.203125],window='hamming')
+    beta=signal.firwin(9,[0.203125,0.46875],window='hamming')
+    filtedData=signal.filtfilt(overall,1,raw)
+    filtedtheta=signal.filtfilt(theta,1,filtedData)
+    filtedalpha=signal.filtfilt(alpha,1,filtedData)
+    filtedbeta=signal.filtfilt(beta,1,filtedData)
+    ftheta,psdtheta=signal.welch(filtedtheta,nperseg=256)
+    falpha,psdalpha=signal.welch(filtedalpha,nperseg=256)
+    fbeta,psdbeta=signal.welch(filtedbeta,nperseg=256)
+    feature.append(max(psdtheta))
+    feature.append(max(psdalpha))
+    feature.append(max(psdbeta))
+    return feature
+
 
 def driver():
     description = "Biosignal Emotions project BHS 2020."
