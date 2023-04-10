a71db1722a107c77e1956475c02f6eb3ff079c0c
WeiHung Lin <b06207053@ntu.edu.tw>
2023-04-10 22:53:58 +0800

    Build the cypher script with argparse function

--- cypher_script.py
+++ cypher_script.py
@@ -1,2 +1,29 @@
 import useful_functions as UF
+from argparse import ArgumentParser, Namespace
+
+
+parser = ArgumentParser()
+parser.add_argument('-m', '--message', action="store",
+                        help="Path to raw data txt file, i.e. msg_file.txt.", type=str)
+parser.add_argument('-k',"--key", action="store",
+                        help="Please enter your key", type= str)
+parser.add_argument("--mode", action="store", default="enc", type=str,
+                        help="Enc or dec")
+    
+results: Namespace = parser.parse_args()
+
+if __name__ == "__main__":
+
+    if results.mode == "enc":
+        results.mode = "True"
+    elif results.mode == "dec":
+        results.mode = "False"
+
+   
+    handle = open(results.message)
+    for line in handle:
+        line = line.rstrip()
+        print(UF.process_message(line, results.key, results.mode))
+        print(line)
+
 
